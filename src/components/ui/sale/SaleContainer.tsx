627a721e05791bcf8e18debe34d19652f0f903c9
Manik <tonysins43@gmail.com>
2024-01-28 01:36:22 +0600

    make filter

--- src/components/ui/sale/SaleContainer.tsx
+++ src/components/ui/sale/SaleContainer.tsx
@@ -1,11 +1,44 @@
+import { Select } from "antd";
 import { useGetSalesQuery } from "../../../redux/features/sale/saleApi";
+import Loader from "../loader/Loader";
 import SaleHistoryTable from "./SaleHistoryTable";
+import { useState } from "react";
+import { Option } from "antd/es/mentions";
 
 const SaleContainer = () => {
-  const { data, isLoading } = useGetSalesQuery(undefined);
-  console.log(isLoading, data);
+  const [selectedValue, setSelectedValue] = useState(null);
+  const { data, isLoading } = useGetSalesQuery(selectedValue);
+  if (isLoading) {
+    return <Loader />;
+  }
+  // handle onChange
+
+  const handleSelectChange = (value: string) => {
+    setSelectedValue(value);
+    // console.log(value);
+  };
   return (
     <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "end",
+          marginTop: "5px",
+          marginBottom: "5px",
+        }}
+      >
+        <Select
+          style={{ width: 200 }}
+          placeholder="Filter History"
+          onChange={handleSelectChange}
+          value={selectedValue}
+        >
+          <Option value="daily">Daily</Option>
+          <Option value="weekly">Weekly</Option>
+          <Option value="monthly">Monthly</Option>
+          <Option value="yearly">Yearly</Option>
+        </Select>
+      </div>
       <SaleHistoryTable data={data?.data} />
     </div>
   );
--- src/redux/features/sale/saleApi.ts
+++ src/redux/features/sale/saleApi.ts
@@ -3,10 +3,18 @@ import { baseApi } from "../../api/baseApi";
 const saleApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getSales: builder.query({
-      query: () => ({
-        url: "/sales",
-        method: "GET",
-      }),
+      query: (filter) => {
+        console.log(filter);
+        const params = new URLSearchParams();
+        if (filter) {
+          params.append("filter", filter);
+        }
+        return {
+          url: "/sales",
+          method: "GET",
+          params: params,
+        };
+      },
       providesTags: ["sale"],
     }),
     addSale: builder.mutation({
