2f3a635913935b4a2d8fbaf671a2aa316839d1d5
Manik <tonysins43@gmail.com>
2024-01-27 10:46:03 +0600

    make add gift form

--- src/components/ui/addGift/AddGiftContainer.tsx
+++ src/components/ui/addGift/AddGiftContainer.tsx
@@ -8,24 +8,60 @@ const AddGiftContainer = () => {
   const handleAddGiftSubmit = (data: FieldValues) => {
     console.log(data);
   };
+
   return (
     <Row justify={"center"}>
       <GForm onSubmit={handleAddGiftSubmit}>
-        <div style={{ display: "flex", gap: "8px" }}>
+        <div className="md:flex gap-4">
           <GInput type={"text"} name={"name"} label={"Name"} />
           <GInput type={"text"} name={"price"} label={"Price"} />
         </div>
-        <div style={{ display: "flex", gap: "8px" }}>
+        <div className="md:flex gap-4">
           <GInput type={"quantity"} name={"quantity"} label={"Quantity"} />
+          <GSelect
+            name="occasion"
+            label="Occasion"
+            options={[
+              { value: "Birthday", label: "Birthday" },
+              { value: "Anniversary", label: "Anniversary" },
+              { value: "Holiday", label: "Holiday" },
+            ]}
+          />
+        </div>
+        <div className="md:flex gap-4">
+          <GInput type={"text"} name={"recipient"} label={"Recipient"} />
           <GSelect
             name="category"
             label="Category"
             options={[
-              { value: "option1", label: "Option 1" },
-              { value: "option2", label: "Option 2" },
-              { value: "option3", label: "Option 3" },
+              { value: "Home decor", label: "Home decor" },
+              { value: "Gadget", label: "Gadget" },
+              { value: "Accessories", label: "Accessories" },
             ]}
           />
+        </div>
+        <div className="md:flex gap-4">
+          <GSelect
+            name="theme"
+            label="Theme"
+            options={[
+              { value: "Vintage", label: "Vintage" },
+              { value: "Romantic", label: "Romantic" },
+              { value: "Modern", label: "Modern" },
+            ]}
+          />
+          <GSelect
+            name="brand"
+            label="Brand"
+            options={[
+              { value: "GiftCraft", label: "GiftCraft" },
+              { value: "JoyFusion", label: "JoyFusion" },
+              { value: "ElegantGiftery", label: "ElegantGiftery" },
+            ]}
+          />
+        </div>
+        <div style={{ width: "100%" }}>
+          <GInput type={"text"} name={"color"} label={"Color"} />
         </div>
         {/* {errorMessage && (
           <p style={{ color: "red", marginBottom: "7px" }}>{errorMessage}</p>
--- src/pages/SaleHistory.tsx
+++ src/pages/SaleHistory.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SaleHistory = () => {
-  return <div>This is sele history page</div>;
+  return <div className="text-5xl">This is sele history page</div>;
 };
 
 export default SaleHistory;
