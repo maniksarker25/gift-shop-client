7646f300eb14b096c793a6cdc0a3c86bf143967c
Manik <tonysins43@gmail.com>
2024-02-16 14:49:24 +0600

    successfully download invoice pdf

--- src/components/ui/sale/SaleHistoryTable.tsx
+++ src/components/ui/sale/SaleHistoryTable.tsx
@@ -1,3 +1,5 @@
+import { Button } from "antd";
+import jsPDF from "jspdf";
 import moment from "moment";
 
 type TSale = {
@@ -13,21 +15,57 @@ type TSale = {
     color: string;
   };
   quantity: number;
+  totalPrice: number;
   buyerName: string;
+  discountPercentage?: number;
   date: Date;
 };
 
 const SaleHistoryTable = ({ data }: { data: TSale[] }) => {
+  // const downloadPdf = (item) => {
+  //   console.log(item);
+  // };
+  const downloadPdf = (item: TSale) => {
+    const doc = new jsPDF({
+      orientation: "portrait", // or "landscape"
+      unit: "mm", // units: mm, cm, in, px
+      format: [150, 210], // width and height in mm
+    });
+
+    // Add content to the PDF
+    doc.text("Selling Invoice", 55, 10);
+    doc.text(`Gift Name: ${item.giftId.name}`, 10, 20);
+    doc.text(`Quantity: ${item.quantity}`, 10, 30);
+    doc.text(`Price: ${item.giftId.price}`, 10, 40);
+    doc.text(
+      `Discount: ${
+        item?.discountPercentage
+          ? item?.discountPercentage + "%"
+          : "Not Available"
+      }`,
+      10,
+      50
+    );
+    doc.text(`Total Price: ${item.totalPrice}`, 10, 60);
+    doc.text(`Sale Date: ${moment(item.date).format("DD/MM/YYYY")}`, 10, 70);
+
+    // Save PDF
+    doc.save("invoice.pdf");
+  };
+
   return (
-    <div>
+    <div className="min-w-full w-11/12 bg-white  overflow-scroll">
       {" "}
-      <table className="min-w-full bg-white border border-gray-300">
+      <table className="min-w-full w-11/12 bg-white border border-gray-300 overflow-scroll">
         <thead>
           <tr>
             <th className="py-2 px-1 border-b">Gift Name</th>
             <th className="py-2 px-1 border-b">Quantity</th>
             <th className="py-2 px-1 border-b">Price</th>
+            <th className="py-2 px-1 border-b">Discount</th>
+            <th className="py-2 px-1 border-b">Total Price</th>
             <th className="py-2 px-1 border-b">Sale Date</th>
+            <th className="py-2 px-1 border-b">Action</th>
           </tr>
         </thead>
         <tbody className="text-center">
@@ -36,8 +74,19 @@ const SaleHistoryTable = ({ data }: { data: TSale[] }) => {
               <td className="py-2 px-4 border-b">{item?.giftId?.name}</td>
               <td className="py-2 px-4 border-b">{item?.quantity}</td>
               <td className="py-2 px-4 border-b">{item?.giftId?.price}</td>
+              {/* <td className="py-2 px-4 border-b">{item?.giftId?.price}</td> */}
+              <td className="py-2 px-4 border-b">
+                {item?.discountPercentage
+                  ? item?.discountPercentage + "%"
+                  : "Not Available"}
+              </td>
               <td className="py-2 px-4 border-b">
                 {moment(item?.date).format("DD/MM/YYYY")}
+              </td>
+              <td className="py-2 px-4 border-b ">
+                <Button onClick={() => downloadPdf(item)}>
+                  Download Invoice
+                </Button>
               </td>
             </tr>
           ))}
